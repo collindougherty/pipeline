e5e8eb123091d21a7a3a17ce333e3147ab7d6e78
Collin Dougherty <collindougherty99@gmail.com>
2023-11-15 16:29:20 -0600

    beautiful, just needs analysis functions in app

--- frontend/app.r
+++ frontend/app.r
@@ -36,60 +36,61 @@ ui <- fluidPage(
            uiOutput("submit_ui"))
   ),
   fluidRow(
-    column(12, tableOutput("table"))
+    column(12, tableOutput("table")),
+    column(12, textOutput("proceedMessage")),
+    column(4, uiOutput("rfButtonUI")),
+    column(4, uiOutput("lrButtonUI")),
+    column(4, uiOutput("kmButtonUI"))
   ),
   fluidRow(
-    column(12, textOutput("proceedMessage"))
+    column(12, tableOutput("analysisResults"))
   )
 )
 
 server <- function(input, output, session) {
   reactiveDf <- reactiveVal()
   showDropdowns <- reactiveVal(FALSE)
+  showButtons <- reactiveVal(FALSE)
+  showAnalysisButtons <- reactiveVal(FALSE)
   
   observeEvent(input$file1, {
     req(input$file1)
-    
     withProgress(message = 'Uploading and processing data...', value = 0, {
       setProgress(value = 0.25)
       df <- read.csv(input$file1$datapath)
-      
       setProgress(value = 0.50)
       recodedDf <- ncdb_recode(df)
-      
       setProgress(value = 0.75)
       dtypes_data <- dtype(recodedDf)
-      
       reactiveDf(dtypes_data)
       setProgress(value = 1)
     })
-    
     showDropdowns(TRUE)
   })
   
   output$x_vars_ui <- renderUI({
     if(showDropdowns()) {
-      selectInput("x_vars", "Choose X Variables:", 
-                  choices = names(reactiveDf()), 
-                  multiple = TRUE)
+      selectInput("x_vars", "Choose X Variables:", choices = names(reactiveDf()), multiple = TRUE)
     }
   })
   
   output$y_var_ui <- renderUI({
     if(showDropdowns()) {
       availableYChoices <- setdiff(names(reactiveDf()), input$x_vars)
-      selectInput("y_var", "Choose Y Variable:", 
-                  choices = availableYChoices, 
-                  multiple = FALSE)
+      selectInput("y_var", "Choose Y Variable:", choices = availableYChoices, multiple = FALSE)
     }
   })
   
-  observeEvent(input$x_vars, {
-    updateSelectInput(session, "y_var", choices = setdiff(names(reactiveDf()), input$x_vars))
+  observe({
+    if(length(input$x_vars) > 0 && !is.null(input$y_var)) {
+      showButtons(TRUE)
+    } else {
+      showButtons(FALSE)
+    }
   })
   
   output$submit_ui <- renderUI({
-    if(showDropdowns()) {
+    if(showButtons()) {
       actionButton("submit", "Submit", class = "btn-primary")
     }
   })
@@ -97,8 +98,57 @@ server <- function(input, output, session) {
   selectedDf <- reactiveVal()
   
   observeEvent(input$submit, {
-    req(input$x_vars, input$y_var, reactiveDf())
     selectedDf(reactiveDf()[, c(input$x_vars, input$y_var), drop = FALSE])
+    showAnalysisButtons(TRUE)
+  })
+  
+  output$proceedMessage <- renderText({
+    if(showAnalysisButtons()) {
+      "If these look correct, choose an analysis method."
+    }
+  })
+  
+  output$rfButtonUI <- renderUI({
+    if(showAnalysisButtons()) {
+      actionButton("rfButton", "Random Forest", class = "btn-primary")
+    }
+  })
+  
+  output$lrButtonUI <- renderUI({
+    if(showAnalysisButtons()) {
+      actionButton("lrButton", "Linear Regression", class = "btn-primary")
+    }
+  })
+  
+  output$kmButtonUI <- renderUI({
+    if(showAnalysisButtons()) {
+      actionButton("kmButton", "K-Means Clustering", class = "btn-primary")
+    }
+  })
+  
+  # Placeholder functions for analyses
+  random_forest <- function(x_vars, y_var, df) { return(data.frame(Result = "Random Forest")) }
+  linear_regression <- function(x_vars, y_var, df) { return(data.frame(Result = "Linear Regression")) }
+  k_means_clustering <- function(x_vars, df) { return(data.frame(Result = "K-Means Clustering")) }
+  
+  analysisResults <- reactiveVal()
+  
+  observeEvent(input$rfButton, {
+    req(selectedDf(), input$x_vars, input$y_var)
+    results <- random_forest(input$x_vars, input$y_var, selectedDf())
+    analysisResults(results)
+  })
+  
+  observeEvent(input$lrButton, {
+    req(selectedDf(), input$x_vars, input$y_var)
+    results <- linear_regression(input$x_vars, input$y_var, selectedDf())
+    analysisResults(results)
+  })
+  
+  observeEvent(input$kmButton, {
+    req(selectedDf(), input$x_vars)
+    results <- k_means_clustering(input$x_vars, selectedDf())
+    analysisResults(results)
   })
   
   output$table <- renderTable({
@@ -106,12 +156,13 @@ server <- function(input, output, session) {
     head(selectedDf())
   })
   
-  output$proceedMessage <- renderText({
-    if(!is.null(selectedDf())) {
-      return("If these look correct, let's proceed to analysis. Otherwise, edit your selection.")
-    }
+  output$analysisResults <- renderTable({
+    req(analysisResults())
+    analysisResults()
   })
 }
 
 shinyApp(ui = ui, server = server)
+
+    
 
